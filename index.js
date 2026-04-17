a2a7936d2fe62fd847ef8e27c96b64657a3af679
Noah Serwin <noahserwin@gmail.com>
2026-04-17 15:08:02 +0000

    added /add and /pow functionality

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ const app = express();
 const port = process.env.PORT || 3000; // Use Codespaces port
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express!" });
+  res.send('Specify /add?x=val1&y=val2, /subtract?x=val1&y=val2, etc.');
+});
+
+app.get("/add", (req, res) => {
+  let sum = parseFloat(req.query.x) + parseFloat(req.query.y);
+  res.json({sum: sum, x: req.query.x, y: req.query.y});
+});
+
+app.get("/pow", (req, res) => {
+  let pow = req.query.x ** req.query.y;
+  res.json({pow: pow, x: req.query.x, y: req.query.y});
 });
 
 app.listen(port, () => {
